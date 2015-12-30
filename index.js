9a8fc6ed0c637e59acfbc88c52d35c7dc4d035fd
unHealthy <chenzhutian1992@gmail.com>
2015-12-30 19:02:00 +0800

    implement the feature

--- index.js
+++ index.js
@@ -1,7 +1,46 @@
+/* global hexo */
 var assign = require('object-assign');
 
 hexo.config.html_minifier = assign({
-  exclude: []
+    exclude: [],
+    removeComments: true,
+    removeCommentsFromCDATA: true,
+    collapseWhitespace: true,
+    collapseBooleanAttributes: true,
+    removeEmptyAttributes: true,
+    minifyJS: true
 }, hexo.config.html_minifier);
 
-hexo.extend.filter.register('after_render:html', require('./lib/filter'));
+hexo.config.css_minifier = assign({
+  exclude: ['*.min.css']
+}, hexo.config.css_minifier);
+
+hexo.config.js_minifier = assign({
+  enable:true,
+  mangle: true,
+  output: {},
+  compress: {},
+  exclude: ['*.min.js']
+}, hexo.config.js_minifier,{
+    fromString: true
+});
+
+// Image minifier
+hexo.config.image_minifier = assign({
+  enabled    : true,
+  interlaced : false,
+  multipass  : false,
+  optimizationLevel: 3,
+  pngquant    : false,
+  progressive : false
+}, hexo.config.image_minifier);
+
+
+var filter = require('./lib/filter');
+hexo.extend.filter.register('after_render:html', filter.optimizeHTML);
+
+hexo.extend.filter.register('after_render:css', filter.optimizeCSS);
+
+hexo.extend.filter.register('after_render:js', filter.optimizeJS);
+
+hexo.extend.filter.register('after_generate', filter.optimizeImage);
