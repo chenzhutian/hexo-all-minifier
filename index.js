047ecff0d8736673a831d4270c5226e55cc57c0a
unHealthy <chenzhutian1992@gmail.com>
2016-01-01 15:28:39 +0800

    fix a bug

--- index.js
+++ index.js
@@ -3,56 +3,56 @@ var assign = require('object-assign');
 
 //module.exports = function (hexo) {
 
-    if (false === hexo.config.hasOwnProperty('all_minifier') || false === hexo.config.all_minifier) {
-    
-
-        // HTML minifier
-        hexo.config.html_minifier = assign({
-            enable: true,
-            exclude: [],
-            removeComments: true,
-            removeCommentsFromCDATA: true,
-            collapseWhitespace: true,
-            collapseBooleanAttributes: true,
-            removeEmptyAttributes: true,
-            minifyJS: true
-        }, hexo.config.html_minifier);
-
-        // Css minifier
-        hexo.config.css_minifier = assign({
-            enable: true,
-            exclude: ['*.min.css']
-        }, hexo.config.css_minifier);
-
-        // Js minifier
-        hexo.config.js_minifier = assign({
-            enable: true,
-            mangle: true,
-            output: {},
-            compress: {},
-            exclude: ['*.min.js']
-        }, hexo.config.js_minifier, {
-                fromString: true
-            });
-
-        // Image minifier
-        hexo.config.image_minifier = assign({
-            enable: true,
-            interlaced: false,
-            multipass: false,
-            optimizationLevel: 3,
-            pngquant: false,
-            progressive: false
-        }, hexo.config.image_minifier);
+if (false === hexo.config.hasOwnProperty('all_minifier') || true === hexo.config.all_minifier) {
 
 
-        var filter = require('./lib/filter');
-        hexo.extend.filter.register('after_render:html', filter.optimizeHTML);
+    // HTML minifier
+    hexo.config.html_minifier = assign({
+        enable: true,
+        exclude: [],
+        removeComments: true,
+        removeCommentsFromCDATA: true,
+        collapseWhitespace: true,
+        collapseBooleanAttributes: true,
+        removeEmptyAttributes: true,
+        minifyJS: true
+    }, hexo.config.html_minifier);
 
-        hexo.extend.filter.register('after_render:css', filter.optimizeCSS);
+    // Css minifier
+    hexo.config.css_minifier = assign({
+        enable: true,
+        exclude: ['*.min.css']
+    }, hexo.config.css_minifier);
 
-        hexo.extend.filter.register('after_render:js', filter.optimizeJS);
+    // Js minifier
+    hexo.config.js_minifier = assign({
+        enable: true,
+        mangle: true,
+        output: {},
+        compress: {},
+        exclude: ['*.min.js']
+    }, hexo.config.js_minifier, {
+            fromString: true
+        });
 
-        hexo.extend.filter.register('after_generate', filter.optimizeImage);
-    }
+    // Image minifier
+    hexo.config.image_minifier = assign({
+        enable: true,
+        interlaced: false,
+        multipass: false,
+        optimizationLevel: 3,
+        pngquant: false,
+        progressive: false
+    }, hexo.config.image_minifier);
+
+
+    var filter = require('./lib/filter');
+    hexo.extend.filter.register('after_render:html', filter.optimizeHTML);
+
+    hexo.extend.filter.register('after_render:css', filter.optimizeCSS);
+
+    hexo.extend.filter.register('after_render:js', filter.optimizeJS);
+
+    hexo.extend.filter.register('after_generate', filter.optimizeImage);
+}
 //}
