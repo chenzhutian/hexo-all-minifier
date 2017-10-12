ecac772e2c6a7b86490cc2f534e7a7cc23b081c7
zhutian <zhutian.chen@outlook.com>
2017-10-13 00:21:25 +0800

    test: improve the unit test

--- test/concatJS.test.js
+++ test/concatJS.test.js
@@ -11,18 +11,19 @@ const format = require('./utils').hexoPathFormat;
 // Configure.
 const fixtures = [];
 const files = fs.readdirSync(path.resolve(__dirname, './fixture'));
+const htmls = {};
 for (const file of files) {
   fixtures.push(format(file));
+  if (file.endsWith('.html') || file.endsWith('.js')) {
+    htmls[file] = fs.readFileSync(path.resolve(__dirname, './fixture', file));
+  }
 }
 
 // Stub hexo.route.
 const hexoRoute = {
-  buffer: fixtures.reduce((o, file) => {
-    o[file] = true;
-    return o;
-  }, {}),
+  buffer: null,
   get(name) {
-    if(this.buffer[this.format(name)]) {
+    if (this.buffer[this.format(name)]) {
       return fs.createReadStream(path.join(__dirname, './fixture', this.format(name)));
     }
     return undefined;
@@ -40,6 +41,13 @@ const hexoRoute = {
 };
 
 describe('ConcatJS', () => {
+  beforeEach(() => {
+    hexoRoute.buffer = fixtures.reduce((o, file) => {
+      o[file] = htmls[file];
+      return o;
+    }, {});
+  });
+
   it('should do nothing if options.enable is false', () => {
     const hexo = {
       config: {
@@ -51,34 +59,103 @@ describe('ConcatJS', () => {
     expect(concatJS.call(hexo)).to.be.undefined;
   });
 
-  it('should concat all local js', () => {
-    // Configure.
-    const hexo = {
-      config: {
-        js_concator: {
-          enable: true,
-          bundlePath: '//js/bundle.js',
+  describe('when dealing with js', () => {
+      // Configure.
+      const hexo = {
+        config: {
+          js_concator: {
+            enable: true,
+            bundlePath: '//js/bundle.js',
+          }
+        },
+        route: hexoRoute,
+      };
+
+    it('should not touch the remote scripts', () => {
+
+      const promise = concatJS.call(hexo);
+      return promise.then(() => {
+        for (const file of fixtures) {
+          if (file.includes('.html')) {
+            const $raw = cheerio.load(htmls[file]);
+            const expectRemoteScripts = [];
+            $raw('script[src]').each((idx, ele) => {
+              const $script = $raw(ele);
+              const src = $script.attr('src');
+              if (src.startsWith('//') || src.startsWith('http')) {
+                expectRemoteScripts.push(src);
+              }
+            });
+
+            const $ = cheerio.load(hexoRoute.buffer[file]);
+            const actualRemoteScripts = [];
+            $('script[src]').each((idx, ele) => {
+              const $script = $(ele);
+              const src = $script.attr('src');
+              if (src.startsWith('//') || src.startsWith('http')) {
+                actualRemoteScripts.push(src);
+              }
+            });
+            expect(actualRemoteScripts).to.be.deep.equal(expectRemoteScripts);
+          }
         }
-      },
-      route: hexoRoute,
-    };
-    const promise = concatJS.call(hexo);
-    return promise.then(() => {
-      for (const file of fixtures) {
-        if (file.includes('.html')) {
-          const $ = cheerio.load(hexoRoute.buffer[file]);
-          $('script[src]').each((idx, ele) => {
-            const $script = $(ele);
-            const src = $script.attr('src');
-            if(src !== hexo.config.js_concator.bundlePath) {
-              expect(src.startsWith('//') || src.startsWith('http')).to.be.true;
-            }
-          });
-        } else if (file.includes('.js')) {
-          expect(hexoRoute.buffer[format(file)], 'js file has been removed').to.be.undefined;
-        }
-        expect(hexoRoute.buffer[format(hexo.config.js_concator.bundlePath)]).to.has.length.greaterThan(0);
-      }
+      });
     });
-  });
+
+    it('should not concat scripts which exist in some htmls (which have local scripts)', () => {
+
+      const promise = concatJS.call(hexo);
+      return promise.then(() => {
+        for (const file of fixtures) {
+          if (file.includes('.html')) {
+            if (file === 'concatJS1.html') {
+              const $ = cheerio.load(hexoRoute.buffer[file]);
+              const srcs = [];
+              $('script[src]').each((idx, ele) => {
+                const $script = $(ele);
+                const src = $script.attr('src');
+                srcs.push(src);
+              });
+              expect(srcs.some(src => src.startsWith('/script2.js'))).to.be.true;
+            }
+          } else if (file.includes('script2.js')) {
+            expect(hexoRoute.buffer[format(file)], 'js file has been removed').to.be.ok;
+          }
+        }
+      });
+    });
+
+    it('should concat scripts which exist in all htmls (which have local scripts)', () => {
+
+      const promise = concatJS.call(hexo);
+      return promise.then(() => {
+        for (const file of fixtures) {
+          if (file.includes('.html')) {
+            if (file === 'concatJS1.html' || file === 'concatJS2.html') {
+              const $ = cheerio.load(hexoRoute.buffer[file]);
+              const srcs = [];
+              $('script[src]').each((idx, ele) => {
+                const $script = $(ele);
+                const src = $script.attr('src');
+                srcs.push(src);
+              });
+              expect(srcs).contains(format(hexo.config.js_concator.bundlePath));
+            } else {
+              const $ = cheerio.load(hexoRoute.buffer[file]);
+              const srcs = [];
+              $('script[src]').each((idx, ele) => {
+                const $script = $(ele);
+                const src = $script.attr('src');
+                srcs.push(src);
+              });
+              expect(srcs).does.not.contains(format(hexo.config.js_concator.bundlePath));
+            }
+          } else if (file.includes('script1.js')) {
+            expect(hexoRoute.buffer[format(file)], 'js file has been removed').to.be.undefined;
+          }
+          expect(hexoRoute.buffer[format(hexo.config.js_concator.bundlePath)]).to.has.length.greaterThan(0);
+        }
+      });
+    });
+  })
 });
