bc12baf4d4d37fb46fb1c57bd77f34e94fa0c67f
curbengh <43627182+curbengh@users.noreply.github.com>
2019-08-05 22:10:18 +0930

    test: remove array conversion tests

--- test/optimizeJS.test.js
+++ test/optimizeJS.test.js
@@ -19,24 +19,6 @@ describe('OptimizeJS', () => {
     expect(jsMinifier.call(hexo)).to.be.undefined;
   });
 
-  describe('exclude options', () => {
-    it('should warp the exclude to an array if it is not an array', () => {
-      const hexo = {
-        config: {
-          js_minifier: {
-            enable: true,
-            exclude: 'src/**/*'
-          }
-        }
-      };
-      const str = 'strstr';
-      const datas = [{ path: 'src/usr/absolute' }, { path: 'src/test.txt' }];
-      for (const data of datas) {
-        expect(jsMinifier.call(hexo, str, data)).to.deep.equal(str);
-      }
-    });
-  });
-
   it('should minify js', () => {
     const hexo = {
       config: {
